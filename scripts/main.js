838389ebefc9dcd6bf312e5657512a8c25e19923
FarhodD <farhoddavlatov0120@gmail.com>
2023-05-14 22:09:17 +0500

    main section

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
 // SCRIPTS
+// BURGER
+const menuBtn = document.querySelector(".burger__menu"),
+  submenu = document.querySelector(".menu");
+
+function toggleMenu() {
+  menuBtn.classList.toggle("active");
+  submenu.classList.toggle("vis__menu");
+}
