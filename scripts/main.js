87d34c5696eee62559b3e82c68b1d4d6c1b43e3a
mrGreatProgrammer <fhgf9048@gmail.com>
2023-05-15 22:42:17 +0500

    slider, porfolio section

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,23 @@ const menuBtn = document.querySelector(".burger__menu"),
   submenu = document.querySelector(".menu");
 
 function toggleMenu() {
-  menuBtn.classList.toggle("active");
+  menuBtn.classList.toggle("opened-burger");
   submenu.classList.toggle("vis__menu");
 }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    // clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
